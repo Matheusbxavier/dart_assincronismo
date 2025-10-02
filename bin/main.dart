2d3f2a9fb1639d10c0670530fa63fdcb446666a3
Matheus Batista Xavier <101474699+Matheusbxavier@users.noreply.github.com>
2025-10-02 12:10:35 -0300

    commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,13 @@
+import 'package:http/http.dart';
+
 void main() {
-  print("Olá Mundo!");
+  //print("Olá Mundo!");
+  requestData();
+  
+}
+
+requestData(){
+  String url = "https://gist.githubusercontent.com/Matheusbxavier/0570c3091694b0a415cdc1ddd8b89355/raw/dcaba5577bcca0e2f91e9e1916f0de72ce556fdd/accounts.json";
+  Future<Response> futureResponse = get(Uri.parse(url));
+  print(futureResponse);
 }
