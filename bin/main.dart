aa643f886bdfd2ad23e917fe8db8dafd517c7d29
Matheus Batista Xavier <101474699+Matheusbxavier@users.noreply.github.com>
2025-10-05 12:25:40 -0300

    commit

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 
 void main() {
   //print("Olá Mundo!");
-  requestData();
+  //requestData();
+  requestDataAsync();
 }
 
 requestData() {
@@ -21,4 +22,16 @@ requestData() {
     );
     print(mapCarla["balance"]);
   });
+
+  print("Ultima coisa a acontecer na função.");
+}
+
+requestDataAsync() async {
+  String url =
+      "https://gist.githubusercontent.com/Matheusbxavier/0570c3091694b0a415cdc1ddd8b89355/raw/dcaba5577bcca0e2f91e9e1916f0de72ce556fdd/accounts.json";
+
+  Response response = await get(Uri.parse(url));
+  print((json.decode(response.body)[0]));
+  print("De fato, a última coisa a acontecer na função.");
+
 }
